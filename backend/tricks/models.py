2c66c7b4abf0be2026be1bc307ceb7ef169bd4df
farzadoxo <farzadebrahimi.oxo@gmail.com>
2025-07-17 16:19:28 +0330

    - models and form added (uncomplete)

--- backend/tricks/models.py
+++ backend/tricks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from games.models import Game
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Trick(models.Model):
+    Title = models.CharField(max_length=40)
+    Descreption = models.CharField(max_length=5000)
+    Game = models.ForeignKey(Game , on_delete=models.CASCADE)
+    Creator = models.ForeignKey(User , on_delete=models.CASCADE)
