ee15316002b5c64da2aaa5d02530aab48a736357
farzadoxo <farzadebrahimi.oxo@gmail.com>
2025-07-17 13:32:38 +0330

    - publish date field updated to DateField

--- backend/games/models.py
+++ backend/games/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Game(models.Model):
     Title = models.CharField(max_length=20)
     Publisher = models.CharField(max_length=40)
-    PublishDate = models.DateTimeField()
+    PublishDate = models.DateField()
     Genere = models.CharField(max_length=10)
     ESRB = models.CharField(max_length=1)
     IconUrl = models.CharField(max_length=100)
