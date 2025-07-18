267bcff8d7de04f2f057ef530baafff72fd12ef5
farzadoxo <farzadebrahimi.oxo@gmail.com>
2025-07-18 21:16:47 +0330

    - new trick - all tricks - and trick info endpoint urls and movies added

--- backend/tricks/urls.py
+++ backend/tricks/urls.py
@@ -4,5 +4,7 @@ from .views import Tricks
 
 
 urlpatterns = [
-    path('all/',Tricks.get_all_tricks , name='get_all_tricks')
+    path('all/',Tricks.get_all_tricks , name='get_all_tricks'),
+    path('game/<int:game_id>/',Tricks.get_tricks_by_game , name='get_tricks_by_game'),
+    path('new/',Tricks.new_trick , name='new_trick')
 ]
