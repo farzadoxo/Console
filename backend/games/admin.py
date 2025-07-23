acc246782e90b6b3b02c3cb73ae76eb790035ff1
farzadoxo <farzadebrahimi.oxo@gmail.com>
2025-07-23 20:16:50 +0330

    -  games_game table problem fixed

--- backend/games/admin.py
+++ backend/games/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Game
+from .models import Game , Publisher , Platform , ESRB , Genre
 
 
 admin.site.register(Game)
+admin.site.register(Publisher)
+admin.site.register(Platform)
+admin.site.register(ESRB)
+admin.site.register(Genre)
