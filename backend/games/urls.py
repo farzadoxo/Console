7a716a1005ee2e1123aecdb93be1791e56416f73
farzadoxo <farzadebrahimi.oxo@gmail.com>
2025-07-25 21:21:19 +0330

    - get games by added

--- backend/games/urls.py
+++ backend/games/urls.py
@@ -4,5 +4,8 @@ from .views import Games
 
 urlpatterns = [
     path('all/',Games.get_all_games , name='get_all_games'),
-    path('info/?game_id=<int:game_id>' , Games.game_info , name='game_info')
+    path('info/?game_id=<int:game_id>' , Games.game_info , name='game_info'),
+    path('genre/<int:genre_id>/',Games.get_games_by_genre , name='get_games_by_genre'),
+    path('publisher/<int:publisher_id>/',Games.get_games_by_publisher , name='get_games_by_publisher'),
+    path('esrb/<str:esrb_sign>/',Games.get_games_by_esrb , name='get_games_by_esrb'),
 ]
--- backend/games/views.py
+++ backend/games/views.py
@@ -1,13 +1,38 @@
 from django.shortcuts import render
-from .models import Game
+from .models import Game , Genre , Publisher , ESRB
 
 
 class Games:
+    """"""
+
+    def get_games_by_genre(request,genre_id:int):
+        genres = Genre.objects.get(id=genre_id)
+        if genres != None:
+            games = Game.objects.filter(genre__id = genre_id)
+
+            return render(request,'game_by.html',context={'games':games , 'title':"Genre"})
+
+
+    def get_games_by_publisher(request,publisher_id:int):
+        publisher = Publisher.objects.get(id=publisher_id)
+        if publisher != None:
+            games = Game.objects.filter(publisher__id = publisher_id)
+
+            return render(request,'game_by.html',context={'games':games,'title':"Publisher"})
+        
+    
+    def get_games_by_esrb(request,esrb_sign:str):
+        esrb = ESRB.objects.get(sign=esrb_sign)
+        if esrb != None:
+            games = Game.objects.filter(esrb__id = esrb.id)
+
+            return render(request,'game_by.html', context={'games':games , 'title':"ESRB"})
+        
+
+
 
     def get_all_games(request):
         all_games = Game.objects.all()
-        for i in all_games:
-            print(f"{i.title} {type(i)}")
         return render(request,'all_games.html',context={'games':all_games})
     
 
