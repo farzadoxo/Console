d7cc479b3898c0c94623173f1f85c1bf584e912e
farzadoxo <farzadebrahimi.oxo@gmail.com>
2025-09-11 17:46:39 +0330

    - form game choise deleted

--- backend/tricks/forms.py
+++ backend/tricks/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from .extentions import game_choise_field
 from .models import Trick
 
 
@@ -7,7 +6,6 @@ from .models import Trick
 class NewTrickForm(forms.Form):
     title = forms.CharField(label="✍🏻 Title :")
     description = forms.CharField(label="📝 Description :",widget=forms.Textarea({'rows':10 , 'cols':60}))
-    game = forms.ChoiceField(choices=game_choise_field(),label="🎮 Game :")
 
 
 
@@ -17,4 +15,3 @@ class UpdateTrickForm(forms.Form):
     #     fields : 
     title = forms.CharField(label="✍🏻 Title :")
     description = forms.CharField(label="📝 Description :",widget=forms.Textarea({'rows':10 , 'cols':60}))
-    game = forms.ChoiceField(choices=game_choise_field(),label="🎮 Game :")
