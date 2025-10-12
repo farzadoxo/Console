64712049b80a57b109a209885188f4b1e60c8861
farzadoxo <farzadebrahimi.oxo@gmail.com>
2025-10-12 19:01:39 +0330

    Register get fucked

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -6,52 +6,10 @@ from django.http import HttpResponse
 from django.contrib.auth import login , authenticate , logout
 from django.core.exceptions import ObjectDoesNotExist
 from core.messages import MessageMaker as Message
+from extentions import password_checker
 
 
 class Auth:
-
-    def register(request):
-        if request.method == 'POST':
-            form = UserRegisterForm(request.POST)
-
-            if form.is_valid():
-
-                # Check user exist or not
-                if User.objects.filter(email=form.cleaned_data['Email']).count() == 0:
-                    if User.objects.filter(username=form.cleaned_data['UserName']).count() == 0:
-                        try:
-                            
-                            # Create and save user
-                            cd = form.cleaned_data
-                            user = User.objects.create_user(username=cd['username'], email =cd['email'], password=cd['password'])
-                            user.first_name = cd['firstname']
-                            user.last_name = cd['lastname']
-                            user.save()
-                            
-                            # Show message and return 
-                            Message.Auth.user_registred_success(request,cd['username'])
-                            return redirect('home:home')
-                        
-                        except Exception as e:
-                            # Show ERROR and return
-                            Message.Core.error(request,e)
-                            return redirect('authentication:register')
-                    
-                    else:
-                        Message.Auth.username_taken(request)
-                        return redirect('home:home')
-                
-                else:
-                    Message.Auth.email_used_before(request)
-                    return redirect('home:home')
-  
-        else:
-            # Set request method to: GET
-            form= UserRegisterForm()
-        # Render html file
-        return render(request,'register.html',context={'form':form})
-    
-
 
     def login(request):
         if request.method == 'POST':
