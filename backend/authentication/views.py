7c43258c632d87325ad67d0bc604890e963dffe3
farzadoxo <farzadebrahimi.oxo@gmail.com>
2025-10-14 08:40:41 +0330

    - refactor: authentication:register _ endpoint refactored

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -6,10 +6,55 @@ from django.http import HttpResponse
 from django.contrib.auth import login , authenticate , logout
 from django.core.exceptions import ObjectDoesNotExist
 from core.messages import MessageMaker as Message
-from extentions import password_checker
+from .extentions import password_checker
 
 
 class Auth:
+
+    def register(request):
+        if request.method == 'POST':
+            form = UserRegisterForm(request.POST)
+            
+            if form.is_valid():
+                if password_checker(password=form.cleaned_data['password']):
+                    # Check user exist or not
+                    if User.objects.filter(email=form.cleaned_data['email']).count() == 0:
+                        if User.objects.filter(username=form.cleaned_data['username']).count() == 0:
+                            try:
+
+                                # Create and save user
+                                cd = form.cleaned_data
+                                user = User.objects.create_user(username=cd['username'], email =cd['email'], password=cd['password'])
+                                user.first_name = cd['firstname']
+                                user.last_name = cd['lastname']
+                                user.save()
+
+                                # Show message and return 
+                                Message.Auth.user_registred_success(request,cd['username'])
+                                return redirect('home:home')
+
+                            except Exception as e:
+                                # Show ERROR and return
+                                Message.Core.error(request,e)
+                                return redirect('authentication:register')
+
+                        else:
+                            Message.Auth.username_taken(request)
+                            return redirect('home:home')
+
+                    else:
+                        Message.Auth.email_used_before(request)
+                        return redirect('home:home')
+                else:
+                    Message.Auth.password_invalid(request)
+                    return redirect('authentication:register')
+            
+        else:
+            # Set request method to: GET
+            form= UserRegisterForm()
+        # Render html file
+        return render(request,'register.html',context={'form':form})
+
 
     def login(request):
         if request.method == 'POST':
@@ -29,7 +74,7 @@ class Auth:
                 
         else:
             form = UserLoginForm()
-        
+    
         return render(request,'login.html',context={'form':form})
 
 
