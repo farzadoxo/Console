cfc418e794f05a10c25c2036e2cc5a93347eaea7
farzadoxo <farzadebrahimi.oxo@gmail.com>
2025-10-21 09:05:00 +0330

    - publisher in progress - broken

--- backend/publishers/models.py
+++ backend/publishers/models.py
@@ -3,4 +3,5 @@ from django.db import models
 class Publisher(models.Model):
     name = models.CharField(max_length=15)
     estabilishmenDate = models.DateField()
+    about = models.CharField(max_length=2000)
     logoUrl = models.CharField(max_length=100)
--- backend/publishers/views.py
+++ backend/publishers/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render , redirect
 from .models import Publisher
-
-
+from django.core.exceptions import ObjectDoesNotExist
+from core.messages import MessageMaker as Message
 
 class Publishers:
-    ...
+    
+    def get_all_publishers(request):
+        publishers = Publisher.objects.all()
+        return render(request,'all_publishers.html',context={'publishers':publishers})
+    
+
+
+    def show_publisher(request,publisher_id:int):
+        try:
+            publisher = Publisher.objects.get(id=publisher)
+        except ObjectDoesNotExist:
+            Message.Publishers.publisher_does_not_exist(request)
+            return redirect('publishers:all')
+        
+        return render(request,'show_publisher.html',context={'publisher':publisher})
+    
+
+    
