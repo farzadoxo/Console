1e57b943546ba790e1090de5482195c3b3d8c088
farzadoxo <farzadebrahimi.oxo@gmail.com>
2025-11-27 10:38:36 +0330

    - publisher app >>> DRF

--- backend/publishers/views.py
+++ backend/publishers/views.py
@@ -2,23 +2,37 @@ from django.shortcuts import render , redirect
 from .models import Publisher
 from django.core.exceptions import ObjectDoesNotExist
 from core.messages import MessageMaker as Message
+from .serializers import PublisherSerializer
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
-class Publishers:
+
+
+
+class PublisherViewSet(ReadOnlyModelViewSet):
+    serializer_class = PublisherSerializer
+    queryset = Publisher.objects.all()
+
+
+
+
+
+
+# class Publishers:
     
-    def get_all_publishers(request):
-        publishers = Publisher.objects.all()
-        return render(request,'all_publishers.html',context={'publishers':publishers})
+#     def get_all_publishers(request):
+#         publishers = Publisher.objects.all()
+#         return render(request,'all_publishers.html',context={'publishers':publishers})
     
 
 
-    def show_publisher(request,publisher_id:int):
-        try:
-            publisher = Publisher.objects.get(id=publisher)
-        except ObjectDoesNotExist:
-            Message.Publishers.publisher_does_not_exist(request)
-            return redirect('publishers:all')
+#     def show_publisher(request,publisher_id:int):
+#         try:
+#             publisher = Publisher.objects.get(id=publisher)
+#         except ObjectDoesNotExist:
+#             Message.Publishers.publisher_does_not_exist(request)
+#             return redirect('publishers:all')
         
-        return render(request,'show_publisher.html',context={'publisher':publisher})
+#         return render(request,'show_publisher.html',context={'publisher':publisher})
     
 
     
