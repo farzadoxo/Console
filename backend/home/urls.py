4891e4cd2a75a4bdd747b56a7976d2096e4f38e0
farzadoxo <farzadebrahimi.oxo@gmail.com>
2025-12-03 19:50:35 +0330

    - home app removed

--- backend/home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import Home
-
-
-urlpatterns = [
-    path('',Home.home , name='home'),
-    path('profile/<int:user_id>',Home.show_profile , name="show_profile")
-]
