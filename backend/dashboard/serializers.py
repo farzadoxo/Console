b483aa180c6c108e91d5b446734a5123a4c2ba33
farzadoxo <farzadebrahimi.oxo@gmail.com>
2025-12-18 16:51:40 +0330

    - saved tricks added

--- backend/dashboard/serializers.py
+++ backend/dashboard/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer , ValidationError
 from .models import FavoritGame , SavedTrick
 from django.contrib.auth.models import User
 
@@ -14,6 +14,12 @@ class SavedTrickSerializer(ModelSerializer):
     class Meta:
         model = SavedTrick
         fields = '__all__'
+        read_only_fields =('user',)
+
+        def validate_exist(self,value):
+            if SavedTrick.objects.filter(trick__id=value).exists():
+                raise ValidationError("This trick saved before!")
+            return value
 
 
 class ProfileSerializer(ModelSerializer):
--- backend/dashboard/views.py
+++ backend/dashboard/views.py
@@ -1,4 +1,4 @@
-from rest_framework.viewsets import ModelViewSet,ReadOnlyModelViewSet
+from rest_framework.viewsets import ModelViewSet,ViewSet
 from rest_framework.views import APIView
 from .serializers import FavoritGameSerializer , SavedTrickSerializer , ProfileSerializer
 from .models import FavoritGame , SavedTrick
@@ -11,9 +11,34 @@ from rest_framework.permissions import IsAuthenticated
 
 
 
-class SavedTrickViewSet(ModelViewSet):
-    serializer_class = SavedTrick
-    queryset = SavedTrick.objects.all()
+class SavedTrickViewSet(ViewSet):
+    permission_classes = [IsAuthenticated]
+
+
+
+    def create(self,request):
+        serializer = SavedTrickSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data , status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+
+    def list(self,request):
+        saved = SavedTrick.objects.filter(user = request.user)
+        serializer = SavedTrickSerializer(saved,many=True)
+
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    
+
+    def retrieve(self,request,pk):
+        trick = SavedTrick.objects.get(id=pk)
+        if trick.user.id == request.user.id:
+            serializer = SavedTrickSerializer(trick)
+
+            return Response(serializer.data , status=status.HTTP_200_OK)
+        return Response("This saved trick is not for you!",status=status.HTTP_400_BAD_REQUEST)
+
 
     def update(self, request, *args, **kwargs):
         raise MethodNotAllowed('PUT')
