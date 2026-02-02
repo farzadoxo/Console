173ccc22243faa9f059e98eb61a434a693aded3f
farzadoxo <farzadebrahimi.oxo@gmail.com>
2026-02-02 16:08:13 +0330

    - manualy query fixed

--- backend/config/init_db.py
+++ backend/config/init_db.py
@@ -36,25 +36,29 @@ def init_db(db_name: str = "db.sqlite3"):
                                 ( 4 , 'Shooter'),
                                 ( 5 , 'Sports')""")
 
-        cursor.execute(f"""INSERT INTO publishers_publisher 
+        cursor.execute(f"""INSERT INTO publishers_publisher (id , name , estabilishmenDate , about , logoUrl)
                         VALUES ( 1 , 'Rockstar Games' , '{datetime.datetime.now()}' , 'This company started at september', '/'),
                                 ( 2 , 'EA' , '{datetime.datetime.now()}' , 'This company started at september' , '/'),
                                 ( 3 , 'Sony' , '{datetime.datetime.now()}' , 'This company started at september' , '/'),
                                 ( 4 , 'Microsoft' , '{datetime.datetime.now()}' , 'This company started at september' , '/'),
                                 ( 5 , 'Konami' , '{datetime.datetime.now()}' , 'This company started at september' , '/')""")
 
-        cursor.execute(f"""INSERT INTO platforms_platform 
-                        VALUES ( 1 , 'Playstation5' , '{datetime.date.today().strftime('%Y-%m')}' , 'This platform created by bill gates in 1998 and release in 2000' , '/', 3),
-                                ( 2 , 'Xbox series X' , '{datetime.date.today().strftime('%Y-%m')}' , 'This gaming console created by sont co at japan' ,'/', 4)""")
+        cursor.execute(f"""INSERT INTO platforms_platform (id , name , releaseDate , publisher_id , about , picUrl)
+                        VALUES ( 1 , 'Playstation5' , '{datetime.date.today().strftime('%Y-%m')}' , 3 , 'This platform created by bill gates in 1998 and release in 2000' , '/'),
+                                ( 2 , 'Xbox series X' , '{datetime.date.today().strftime('%Y-%m')}' , 4 , 'This gaming console created by sont co at japan' ,'/')""")
         
 
-        cursor.execute(f"""INSERT INTO games_game
-                        VALUES ( 1 , 'Red dead redemption 2' , '{datetime.datetime.now()}' , '/' , 2 , 1 , 1),
-                                ( 2 , 'FC25' , '{datetime.datetime.now()}' ,'/' , 5 , 2 , 5)""")
+        cursor.execute(f"""INSERT INTO games_game (id , title , publishDate , iconUrl , publisher_id , genre_id , esrb_id)
+                        VALUES ( 1 , 'Red dead redemption 2' , '{datetime.datetime.now()}' , '/' , 1 , 1 , 2),
+                                ( 2 , 'FC25' , '{datetime.datetime.now()}' ,'/' , 2 , 5 , 5)""")
         
 
-        cursor.execute(f"""INSERT INTO tricks_trick
+        cursor.execute(f"""INSERT INTO tricks_gametrick (id , title , description , createdAt ,game_id , creator_id)
                         VALUES ( 1 , 'how to play the rdr2' ,'sl;dkfsl;dfl;skdf;lks;ldkf;sldkf;ksdkf;;', '{datetime.datetime.now()}' , 1 , 1)""")
+        
+        
+        cursor.execute(f"""INSERT INTO tricks_platformtrick (id , title , description , createdAt ,platform_id , creator_id)
+                        VALUES ( 1 , 'how to install game on ps5' ,'sl;dkfsl;dfl;skdf;lks;ldkf;sldkf;ksdkf;;', '{datetime.datetime.now()}' , 1 , 1)""")
         
         
         cursor.connection.commit()
